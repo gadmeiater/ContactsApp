6e8177dec014e7708471a2fd4f5f774e207994a1
artemgaluza0@gmail.com <artemgaluza0@gmail.com>
2021-11-01 23:31:25 +0700

    Добавлен проект логики

--- ContactsAppUI/MainForm.cs
+++ ContactsAppUI/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsApp;
 
 namespace ContactsAppUI
 {
@@ -15,6 +16,11 @@ namespace ContactsAppUI
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
